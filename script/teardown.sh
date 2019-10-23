4c46f227464b85edbb85823a454b0bb6ae9e3b4d
Steve Winton <stevewinton@gmail.com>
2019-10-23 10:15:49 -0500

    Update scripts

--- script/teardown.sh
+++ script/teardown.sh
@@ -5,7 +5,7 @@ cd $( dirname $0 )/..
 
 source .envrc
 
-aws ecs update-service --cluster $CLUSTER --service nginx --desired-count 0
-aws ecs delete-service --cluster $CLUSTER --service nginx
-aws ecs wait services-stable --cluster $CLUSTER --services nginx
+aws ecs update-service --cluster $CLUSTER --service $SERVICE --desired-count 0
+aws ecs delete-service --cluster $CLUSTER --service $SERVICE
+aws ecs wait services-inactive --cluster $CLUSTER --services $SERVICE
 aws ecs delete-cluster --cluster $CLUSTER
